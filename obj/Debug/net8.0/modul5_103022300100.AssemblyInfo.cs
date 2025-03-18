c9a5735421714abac24d928a29fde3bc75d58c94
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-18 07:11:21 +0700

    menambahkan class PemrosesanData dengan method DapatkanNilaiTerbesar

--- obj/Debug/net8.0/modul5_103022300100.AssemblyInfo.cs
+++ obj/Debug/net8.0/modul5_103022300100.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul5_103022300100")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f892e3711046db30747bfcb35d665ebd9e7d5d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul5_103022300100")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul5_103022300100")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
